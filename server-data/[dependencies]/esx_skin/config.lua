3189dd0fc94465431c08dbd0b99289fc8e7f5a4b
bitpredator <67551273+bitpredator@users.noreply.github.com>
2022-07-30 16:19:30 +0200

    Add files via upload

---
+++ server-data/[dependencies]/esx_skin/config.lua
@@ -0,0 +1,10 @@
+Config = {}
+
+Config.Locale = 'it'
+
+Config.BackpackWeight = {
+	[40] = 16, 
+	[41] = 20, 
+	[44] = 25, 
+	[45] = 23
+}
---
+++ server-data/[dependencies]/esx_skin/locales/it.lua
@@ -0,0 +1,6 @@
+Locales['it'] = {
+  ['skin_menu'] = 'Skin Menu',
+  ['use_rotate_view'] = 'usa ~INPUT_FRONTEND_LS~ e ~INPUT_CHARACTER_WHEEL~ per ruotare la visuale.',
+  ['skin'] = 'cambia skin',
+  ['saveskin'] = 'salva la skin',
+}
