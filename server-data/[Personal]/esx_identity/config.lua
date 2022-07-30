347cf23fbe93c6ea0f1ac8251212da9a412a63e9
bitpredator <67551273+bitpredator@users.noreply.github.com>
2022-07-30 16:29:55 +0200

    Add files via upload

---
+++ server-data/[Personal]/esx_identity/config.lua
@@ -0,0 +1,19 @@
+Config                  = {}
+Config.Locale           = 'it'
+
+-- [Config.EnableCommands]
+-- Enables Commands Such As /char and /chardel
+Config.EnableCommands   = false
+
+Config.UseDeferrals     = false -- EXPERIMENTAL Character Registration Method.
+
+
+-- These values are for the second input validation in server/main.lua
+Config.MaxNameLength    = 20
+Config.MinHeight        = 120
+Config.MaxHeight        = 220
+Config.LowestYear       = 1900
+Config.HighestYear      = 2021
+
+Config.FullCharDelete   = false
+Config.EnableDebugging  = false
