be6ef5da210786d89cf4e9fdaf5aa4dc3991a731
bitpredator <67551273+bitpredator@users.noreply.github.com>
2022-08-01 18:18:58 +0200

    new loadscreen & fix database

---
+++ server-data/resources/[esx]/GTA-Loadscreen/__resource.lua
@@ -0,0 +1,20 @@
+name "gtav-loadscreen"
+description "gtav-loadscreen"
+author "bexolumu"
+version "1.0.0"
+
+files {
+    'index.html',
+    'config.json',
+    'css/bootstrap.min.css',
+    'css/styles.min.css',
+    'img/bg-1.jpg',
+    'img/bg-2.jpg',
+    'img/loader.gif',
+    'img/logo.png',
+    'js/bootstrap.min.js',
+    'js/jquery-3.6.0.min.js',
+    'js/main.min.js',
+}
+
+loadscreen 'index.html'
